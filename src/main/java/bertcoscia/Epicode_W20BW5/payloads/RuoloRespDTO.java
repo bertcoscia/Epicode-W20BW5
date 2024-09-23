bdc16c14d2affd806f9c322281fc1aa1d638c8e9
Sun/chems Zghaidi <154335653+Chems05@users.noreply.github.com>
2024-09-23 18:31:30 +0200

    finito i payloads

---
+++ src/main/java/bertcoscia/Epicode_W20BW5/payloads/RuoloRespDTO.java
@@ -0,0 +1,11 @@
+package bertcoscia.Epicode_W20BW5.payloads;
+
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class RuoloRespDTO {
+    private UUID id;
+    private String nome;
+
+}
