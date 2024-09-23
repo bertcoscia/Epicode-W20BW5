1255e7ee94feb81c7063e8f8feb226c5346082b1
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-23 12:34:58 +0200

    Initial commit

---
+++ src/main/java/bertcoscia/Epicode_W20BW5/EpicodeW20Bw5Application.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W20BW5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeW20Bw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeW20Bw5Application.class, args);
+	}
+
+}
