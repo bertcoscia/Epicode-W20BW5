47d9b0f977dced4fefcd67dee77df18748cd692d
Simone Pomponio <Pomponio671@gmail.com>
2024-09-23 18:53:02 +0200

    clienti Service

--- src/main/java/bertcoscia/Epicode_W20BW5/services/ClientiService.java
+++ src/main/java/bertcoscia/Epicode_W20BW5/services/ClientiService.java
@@ -1,11 +1,13 @@
 package bertcoscia.Epicode_W20BW5.services;
 
 import bertcoscia.Epicode_W20BW5.entities.Clienti;
+import bertcoscia.Epicode_W20BW5.exceptions.NotFoundException;
 import bertcoscia.Epicode_W20BW5.repositories.ClientiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ClientiService {
@@ -13,13 +15,21 @@ public class ClientiService {
     @Autowired
     private ClientiRepository clientiRepository;
 
-
+    //Find All
     public List<Clienti> findAllClienti() {
         return clientiRepository.findAll();
     }
 
-
-    /*public Clienti findClienteById(UUID clienteId) {
+    // Find by ID
+    public Clienti findClienteById(UUID clienteId) {
         return this.clientiRepository.findById(clienteId).orElseThrow(() -> new NotFoundException(clienteId));
-    }*/
+    }
+
+    // DELETE
+    public void findByClienteIdAndDelete(UUID clienteId) {
+        Clienti found = this.findClienteById(clienteId);
+        this.clientiRepository.delete(found);
+
+
+    }
 }
