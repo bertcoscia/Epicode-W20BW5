17e73b2a6f4db3d4fc0e03b8bfb6c120b8acf33b
Federico Di Presa <federicodipresa94@gmail.com>
2024-09-25 16:20:39 +0200

    mapper

--- src/main/java/bertcoscia/Epicode_W20BW5/mappers/Mapper.java
+++ src/main/java/bertcoscia/Epicode_W20BW5/mappers/Mapper.java
@@ -3,10 +3,12 @@ package bertcoscia.Epicode_W20BW5.mappers;
 import bertcoscia.Epicode_W20BW5.entities.Fattura;
 import bertcoscia.Epicode_W20BW5.DTO.FatturaDTO;
 
-public class FatturaMapper {
 
-    public static FatturaDTO toDto(Fattura fattura) {
-        return new FatturaDTO(
+
+// public class FatturaMapper {
+
+    // public static FatturaDTO toDto(Fattura fattura) {
+        // return new FatturaDTO(
                 fattura.getId(),
                 fattura.getData(),
                 fattura.getImporto(),
