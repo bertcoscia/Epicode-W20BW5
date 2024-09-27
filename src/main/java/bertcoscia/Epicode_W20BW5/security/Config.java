6ae7e1c62d5647d08ae9762255005849f97942ca
Simone Pomponio <Pomponio671@gmail.com>
2024-09-27 12:14:39 +0200

    config frontend

--- src/main/java/bertcoscia/Epicode_W20BW5/security/Config.java
+++ src/main/java/bertcoscia/Epicode_W20BW5/security/Config.java
@@ -2,6 +2,7 @@ package bertcoscia.Epicode_W20BW5.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
